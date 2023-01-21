9b018b18d6a472fece620919f663489241102d74
kostasdak <kostas_dak@yahoo.com>
2023-01-21 11:16:20 -0500

    Doc

--- db.go
+++ db.go
@@ -7,12 +7,14 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+// ConnectDatabase
 func ConnectDatabase(user string, pass string, dbname string) (*sql.DB, error) {
 	cstring := user + ":" + pass + "@/" + dbname
 	db, err := sql.Open("mysql", cstring) // "user:password@/dbname" user+":"+pass+"/"+dbname
 	if err != nil {
 		panic(err)
 	}
+
 	// See "Important settings" section.
 	db.SetConnMaxLifetime(time.Minute * 3)
 	db.SetMaxOpenConns(10)
